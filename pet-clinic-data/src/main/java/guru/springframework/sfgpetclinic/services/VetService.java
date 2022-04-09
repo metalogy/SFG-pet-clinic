80d48f9375421afeeb34e92b90055ed7ff88d0da
metalogy <metalogy@wp.pl>
2022-04-09 21:52:22 +0200

    Common interface services refactor

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgpetclinic.services;
 
-import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.Vet;
-
-import java.util.Set;
 
 public interface VetService extends CrudService<Vet, Long> {
 
