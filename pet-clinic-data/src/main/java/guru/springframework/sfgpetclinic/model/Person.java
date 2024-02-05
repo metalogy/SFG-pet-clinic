6c28d32d378ba14f2cce21d80abcb8d2e69022a6
metalogy <metalogy@wp.pl>
2024-02-05 21:35:16 +0100

    Refactored person entity

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,26 +1,21 @@
 package guru.springframework.sfgpetclinic.model;
 
-/**
- * Created by jt on 7/13/18.
- */
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@Getter
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name")
+    @Setter
     private String firstName;
+
+    @Column(name = "last_name")
+    @Setter
     private String lastName;
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
